e1540b865059cdee47aac4b7e3289d390d67d1a3
florisvb <florisvb@gmail.com>
2012-08-04 12:30:03 -0700

    first real commit

---
+++ analyze.py
@@ -0,0 +1,51 @@
+import sys, os, shutil
+from optparse import OptionParser
+
+import save_h5_to_dataset
+import prep_dataset
+import make_plots
+import generate_empty_data_directory
+
+def main(path, config, options):
+    h5 = options.h5
+    odor = options.odor
+    
+    try:
+        generate_empty_data_directory.main(path, config)
+    except:
+        'Could not make empty data directory -- probably exists already!'
+        return
+        
+    h5_path = os.path.join(path, config.h5_path)
+    shutil.move(h5, h5_path)
+    
+    if odor is not None:
+        odor_path = os.path.join(path, config.odor_control_path)
+        shutil.move(odor, odor_path)
+
+    if config.h5_files is not None:
+        save_h5_to_dataset.main(path, config)
+
+    prep_dataset.main(path, config)
+    make_plots.main(path, config)
+    
+    
+
+if __name__ == '__main__':
+    
+    parser = OptionParser()
+    parser.add_option("--path", type="str", dest="path", default='',
+                        help="path to empty data folder, where you have a configuration file")
+    parser.add_option("--h5", type="str", dest="h5", default='',
+                        help="h5 file you wish to analyze, it will get moved to the new directory")
+    parser.add_option("--odor", type="str", dest="odor", default=None,
+                        help="odor file for h5 file, it will get moved to the new directory")
+                        
+    (options, args) = parser.parse_args()
+    
+    path = options.path
+    sys.path.append(path)
+    import analysis_configuration
+    config = analysis_configuration.Config()
+    
+    main(path, config, options)
---
+++ prep_dataset.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+import sys, os
+from optparse import OptionParser
+
+import flydra_analysis_tools as fat
+fad = fat.flydra_analysis_dataset
+dac = fat.dataset_analysis_core
+tac = fat.trajectory_analysis_core
+
+import analysis_modules.odor_control_analysis as oca
+
+####################### analysis functions ###########################
+def culling_function(raw_dataset):
+    culled_dataset = dac.cull_dataset_min_frames(raw_dataset, min_length_frames=10, reset=True)
+    culled_dataset = dac.cull_dataset_cartesian_volume(culled_dataset, [-.2, .2], [-1,.3], [-.1,.4], reset=True)
+    culled_dataset = dac.cull_dataset_min_speed(culled_dataset, min_speed=0.05, reset=True)
+    return culled_dataset
+
+
+def prep_data(culled_dataset, path, config):
+    # stuff like calculating angular velocity, saccades etc.
+    culled_dataset.info = config.info
+    fad.iterate_calc_function(culled_dataset, tac.calc_local_timestamps_from_strings) # calculate local timestamps
+    
+    if config.odor is True:
+        oca.calc_odor_vs_no_odor_simple(culled_dataset, get_odor_control_filename(path, config))
+    else:
+        fad.set_attribute_for_trajecs(culled_dataset, 'in_odor', False)
+    return    
+    
+def get_odor_control_filename(path, config):
+    
+    odor_control_path = os.path.join(path, config.odor_control_path)
+    if 1: # default to find a file in the path
+        try:
+            cmd = 'ls ' + odor_control_path
+            ls = os.popen(cmd).read()
+            all_filelist = ls.split('\n')
+        except:
+            all_filelist = None
+            print 'Could not find odor control path - might need to make it first'
+        
+        if all_filelist is not None:
+            try:
+                all_filelist.remove('')
+            except:
+                pass
+            if len(all_filelist) == 1:
+                odor_control_filename = all_filelist[0]
+            else:
+                print 'could not find unique odor control file in path'
+            
+    if 0: # hard code the odor control file
+        odor_control_filename = config.odor_control_filename
+        
+    odor_control_filename = os.path.join(path, config.odor_control_path, odor_control_filename)
+        
+    return odor_control_filename
+        
+        
+def main(path, config):
+    
+    # path stuff
+    culled_dataset_name = os.path.join(path, config.culled_datasets_path, config.culled_dataset_name)
+    raw_dataset_name = os.path.join(path, config.raw_datasets_path, config.raw_dataset_name)
+    
+    print 
+    print 'Culling and Preparing Data'
+    
+    try:
+        culled_dataset = fad.load(culled_dataset_name)
+        print 'Loaded culled dataset'
+    except:
+        try:
+            raw_dataset = fad.load(raw_dataset_name)
+            print 'Loaded raw dataset'
+        except:
+            print 'Cannot find dataset, run save_h5_to_dataset.py first'
+                
+        culled_dataset = culling_function(raw_dataset) 
+        print 'Preparing culled dataset'
+        prep_data(culled_dataset, path, config)
+        fad.save(culled_dataset, culled_dataset_name)
+        print 'Saved culled dataset'
+        
+    prep_data(culled_dataset, path, config)
+    
+    return culled_dataset
+    
+if __name__ == '__main__':
+
+    parser = OptionParser()
+    parser.add_option("--path", type="str", dest="path", default='',
+                        help="path to empty data folder, where you have a configuration file")
+    (options, args) = parser.parse_args()
+    
+    path = options.path    
+    sys.path.append(path)
+    import analysis_configuration
+    config = analysis_configuration.Config()
+    
+    culled_dataset = main(path, config)
